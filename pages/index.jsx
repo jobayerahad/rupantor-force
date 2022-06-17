d0fcfc01f1a3cb1e28c882179947bba20503bd3a
Md Jobayer Al Mahmud Ahad <30753487+jobayerahad@users.noreply.github.com>
2022-06-18 01:37:04 +0600

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable @next/next/no-img-element */
+import Head from 'next/head'
+
+const Home = () => (
+  <>
+    <Head>
+      <title>Rupantor Force Security Services Ltd.</title>
+    </Head>
+
+    <main>
+      <img src="/images/coming_soon.svg" alt="coming soon" />
+      <h1>We&apos;re coming soon to operation</h1>
+    </main>
+  </>
+)
+
+export default Home
