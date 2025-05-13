356b20ed289acf65d19e7a90c529145624ad0064
Md Jobayer Al Mahmud Ahad <30753487+jobayerahad@users.noreply.github.com>
2025-05-14 00:06:54 +0600

    og changes

--- src/config/meta.ts
+++ src/config/meta.ts
@@ -3,15 +3,16 @@ import { SharedMetaData } from '@types'
 
 export const sharedMetadata = ({ description, image, path, title }: SharedMetaData): Metadata => ({
   metadataBase: new URL(`https://www.rupantorforce.com/${path}`),
-  title: title,
+  title,
   description: description,
   openGraph: {
-    title: title,
+    title,
     description: description,
     type: 'website',
     url: new URL(`https://www.rupantorforce.com/${path}`),
     countryName: 'Bangladesh',
     determiner: 'the',
-    images: image || '/logo.png'
+    images: image || '/logo.png',
+    siteName: 'Rupantor Force Security Services Limited'
   }
 })
